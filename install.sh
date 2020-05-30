66c645afcf1802469a4621432d60bc8bc4e4efbc
Philipp Bergsmann <me@ph-bergsmann.com>
2020-05-30 23:48:49 +0200

    Adds blank line

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ if ! [ -x "$(command -v hcloud-fip-sync)" ]; then
     cd $TMP_DIR
     curl -Lo hcloud-fip-sync.tar.gz https://github.com/phbergsmann/hcloud-fip-sync/releases/download/$HCLOUD_FIP_SYNC_VERSION/hcloud-fip-sync.tar.gz
     tar -xzvf hcloud-fip-sync.tar.gz
+    
     mv hcloud-fip-sync /usr/bin/
     mv hcloud-fip-sync.service /etc/systemd/system/
     mv hcloud-fip-sync.timer /etc/systemd/system/
